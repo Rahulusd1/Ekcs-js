df623f7599f8c1c519a6c5eb74672d6a50115a75
swwayam <codingswayam24@gmail.com>
2022-11-24 20:04:05 +0530

    Ekcs js setup

---
+++ InputOutputBegg.js
@@ -0,0 +1,24 @@
+// Single line Comment 
+
+// Used For Debugging
+console.log("Hello World!!")
+
+/** Multiline Comment
+ * 
+ */
+
+
+ var value = "This is an message from value var"
+
+// Used for an message box
+alert(value);
+
+// Taking input from user using prompt
+var input = prompt("Please enter a value");
+
+console.log(input)
+
+
+
+
+
---
+++ dataTypes.js
@@ -0,0 +1,5 @@
+var string = "Swayam";   // string data type
+var integer = 8055117107;// integer data type
+var float = 34.45454;    // float data type
+var boolean = true;      // boolean data type 
+
