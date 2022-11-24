6e49a65b25e5b17a2f441fc4779a3a48003a1ed1
swwayam <codingswayam24@gmail.com>
2022-11-24 20:15:46 +0530

    Task 1

---
+++ Task1.js
@@ -0,0 +1,8 @@
+/**
+ * 1. Create 2 variables and perform their addition, sub, multiply,divide and log it to the console.
+ * 
+ * 2. Take input from the user using prompt and alert the value inputed.
+ * 
+ * 3. Take two inputs from the user using prompt and add those two value and log the result to the console
+ */
+
