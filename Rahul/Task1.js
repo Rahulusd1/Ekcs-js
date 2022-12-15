923cfc78804d9d53fc54d731f4e16ec45d48743f
Rahulusd1 <rahulusd1@gmail.com>
2022-12-15 20:59:38 +0530

    Loop file

--- Rahul/Task1.js
+++ Rahul/Task1.js
@@ -5,3 +5,33 @@
  * 
  * 3. Take two inputs from the user using prompt and add those two value and log the result to the console
  */
+
+
+// var num1 = 10;
+// var num2 = 20;
+// var sum = num1 + num2; 
+// var sub = num1 - num2;
+// var div = num1 / num2;
+// var mul = num1 * num2;
+
+// console.log(sum);
+// console.log(sub);
+// console.log(div);
+// console.log(mul);
+
+// var value = "This is an message from value var"
+
+// alert(value);
+
+// var input = prompt("Please enter a value");
+
+// console.log(input)
+
+var num1 = prompt('enter the first number');
+var num2 = prompt('enter the secend number');
+var sum = parseInt(num1) + parseInt(num2);
+console.log(sum);
+
+
+
+
--- Rahul/Task2.js
+++ Rahul/Task2.js
@@ -1,2 +1,16 @@
 // Take age input from the user and check if he is elidgible for a driving liscence.
 // Take name input from the user and check if his first character is a if it is a then say its a or else say its not a
+
+// age = Number(prompt('Please enter here your age'));
+// if(age < 18){
+//     alert("You are not eligible for the Liscence")
+// }else{
+//     alert("You are eligible")
+// }
+
+var user = prompt("Please enter your Name"); 
+if(user[0] == "A"){ 
+    alert("First letter of your name is A");
+}else{
+    alert("First letter of your name is not A")
+}
