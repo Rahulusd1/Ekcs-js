46d7387dc5a2f5ba020c35c615d113efd44e15aa
swwayam <codingswayam24@gmail.com>
2022-12-15 19:40:45 +0530

    loops

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+// name = "rahul"
+// name2 = "sudhir"
+// name3 = "anoop"
+
+// Arrays
+
+var names = ["rahul", "sudir", "Anoop"];
+
+console.log(names[0]);
+
---
+++ Loops.js
@@ -0,0 +1,55 @@
+/* 
+
+There are three types of loops in js
+
+1. For Loop
+2. While Loop
+3. Do While Loop
+
+*/
+
+// // Below we are not following dry principle
+
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+// console.log("I will do my hw");
+
+// // Solution is loop
+
+// // While Loop
+
+// var write = 1;
+
+// while(write <= 100){
+//     console.log(write + " I will do my hw coming from while loop");
+//     write++;
+// }
+
+// // For Loop
+
+// for(var write = 1; write <= 100; write++){
+//     console.log(write + " I will do my hw coming from for loop");
+// }
+
+
+// // Do While Loop
+
+// var num = 8
+
+// do{
+//     console.log(num)
+//     num++;
+// }while(num < 10);
+
+
+
+// Practice of Loops
+
+var name = 'Anoop Kumar';
+
+
