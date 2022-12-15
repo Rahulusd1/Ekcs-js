5b0e0a64eeb16be4cc82c83069eeb24bca0d9988
Sudhir2012 <sdkumar@ekcs.co>
2022-12-15 10:28:34 -0500

    task3

---
+++ Sudhir/Task3.js
@@ -0,0 +1,14 @@
+var names = ["Rahul", "Anoop", "Sudhir"];
+// var len = names.length;
+// for(i = 0; i < len; i++){
+//     console.log(names[i])
+// }
+
+var n1 = 0
+while(n1 <= 2){
+    console.log(names[n1]);
+    n1++;
+}
+
+
+// https://www.geeksforgeeks.org/arrays-in-javascript/
