7c48b58cc9979282809afd96f34f42bee0960021
Rahulusd1 <rahulusd1@gmail.com>
2022-12-22 20:04:22 +0530

    first

--- Tasks/surprise1.js
+++ Tasks/surprise1.js
@@ -1,32 +1,33 @@
-var values = [Field('Name1'), 
-var table = new FPTable;
-table.AddColumns(134000,134000,134000,134000);
-table.AddRows(6);
-table.Rows[0].Type = "";
-table.Rows[0].CopyCells(0,1,2,3);
-var values = [Field('Name1'), Field('Name2'), Field('Name3'), Field('Name4'), Field('Name5'), Field('Name6'), Field('Name7'), Field('Name8')]
+/* 
+Task 1 -
+1. Take 10 inputs from the user.  // Loop
+2. All these inputs should be int 
+3. Store all the inputs in the array. // Store
+4. Alert the sum of all the elements in the array. 
+*/
 
-var filteredValue = values.filter(Boolean)
-for (var i = 1; i <= Math.ceil(filteredValue.length / 4); i++) {
-var row = [];
-var row = filteredValue.slice((i-1)*4, 4 * i)
+var numbers = [];
+var sum = 0;
 
-if (row.length==1){
-table.Rows[i].SetContents(row[0]);
-}
-else if (row.length==2){
-table.Rows[i].SetContents(row[0],row[1]);
-}
-else if (row.length==3){
-table.Rows[i].SetContents(row[0],row[1], row[2]);
-}
-else if (row.length==4){
-table.Rows[i].SetContents(row[0],row[1], row[2], row[3]);
-}
-table.Rows[i].Cells[0].Margins = new FPTableMargins;
-table.Rows[i].Cells[0].Margins.Bottom = 2000;
-table.Rows[i].Cells[0].VAlign = "Center";
-table.Rows[i].Cells[0].HAlign = "Center";
+// Solution 1
 
-}
-return table.MakeTags();
+// for(var input = 1; input <= 10; input++){
+//     var inp = parseInt(prompt("Please enter a number to add"));
+//     numbers.push(inp);
+// }
+
+// for(var i = 0; i < numbers.length; i++){
+//     sum += numbers[i];
+// }
+
+// Solution 2
+
+// for(var input = 1; input <= 10; input++){
+//     var inp = parseInt(prompt("Please enter a number to add"));
+//     numbers.push(inp);
+//     sum += inp;
+// }
+
+
+alert("sum is = " + sum)
+
