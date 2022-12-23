73c8e240d6fce2da5e6a51fd4c3894e73204d4ef
swwayam <codingswayam24@gmail.com>
2022-12-23 20:02:37 +0530

    Classes And Objects

---
+++ objectsAndClasses.js
@@ -0,0 +1,58 @@
+// var arr =[1,34,45,25325]
+
+// Another Data Structure Is Objects
+
+var student = {
+    "name": "Sudhir",
+    "company": "EKCS",
+    "learning": "JS",
+}
+
+console.log(student.company)
+
+student.name = "Ashish";
+
+// methods
+
+var animal = {
+    name: "Tiger",
+    eat: function(){
+        console.log("The animal is eating");
+    }
+}
+
+// var animal1 = {
+//     name: "lion",
+//     eat: function(){
+//         console.log("The animal is eating");
+//     }
+// }
+
+// Instead of Line 24 to 29 you could use classes
+
+// Classses 
+
+// class Animal{
+
+//     constructor(name,numOfLegs,canFly){
+//         this.name = name;
+//         this.numOfLegs = numOfLegs;
+//         this.canFly = canFly;
+//     }
+
+//     eat(){
+//         console.log("The Animal is Eating");
+//     }
+
+//     run(){
+//         console.log("The Animal is running");
+//     }
+
+//     sleep(){
+//         console.log("The Animal is sleeping");
+//     }
+// }
+
+// var tiger = new Animal("Tiger", 4, false);
+
+// var lion = new Animal("Lion",4,true);
