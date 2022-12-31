6933ce5dc588c38d698c10c301a6a9ddd5eb3cc7
Rahulusd1 <rahulusd1@gmail.com>
2022-12-31 13:57:01 +0530

    new

--- Rahul/New folder/main.js
+++ Rahul/New folder/main.js
@@ -25,3 +25,18 @@ function copy(){
     var text = document.getElementById("text").value
     navigator.clipboard.writeText(text)
 }
+
+function firstChar(){
+    var text = document.getElementById("text")
+    var inputValue = text.value
+    var space = inputValue.split(" "); // ["abc", "bc3"]
+    var newArr = [];
+    for(var i = 0; i < space.length; i++){
+        var value = space[i];
+        var sum = value.charAt(0).toUpperCase() + value.slice(1)
+        newArr.push(sum)
+    }
+    var finalValue = newArr.join("  ");
+    console.log(finalValue);
+    text.value = finalValue;
+}
