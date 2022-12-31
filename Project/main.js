77d9f449dab7cec9713225df763330def79878f6
swwayam <codingswayam24@gmail.com>
2022-12-31 12:31:13 +0530

    cp

--- Project/main.js
+++ Project/main.js
@@ -7,3 +7,25 @@ function allCap(){
 
     text.value = upper // UPPER CASE
 }
+
+function allLow(){
+    var text = document.getElementById("text");
+
+    var inputValue = text.value;
+
+    var lower = inputValue.toLowerCase();
+
+    text.value = lower;
+}
+
+function copy(){
+    var text = document.getElementById("text").value
+    navigator.clipboard.writeText(text)
+}
+
+function firstChar(){
+    var text = document.getElementById("text")
+    var inputValue = text.value
+    var space = inputValue.split(" ");
+    console.log(space);
+}
